94d39107dbb21ba9cb6e0eeccad276aeb3adf84e
Mitchell Hudson <soggybag@gmail.com>
2023-01-24 18:57:43 -0800

    Update index.js

--- Promise-5/index.js
+++ Promise-5/index.js
@@ -3,13 +3,15 @@ function whatToEat(timeOfDay) {
   
 }
 
-
-// Call yout function and resolve the promise here! 
-whatToEat('lunch')
-
-
+// whatToEat should return a promise. 
 // If the `timeOfDay` is 'breakfast', 'lunch', or 'dinner' 
 // resolve the promise with your favorite food for that time. 
-
 // If the `timeOfDay` is anything else reject the promise with 
-// the 'message' I'm not hungry right now.  
+// the the message: "I don't eat between meal snacks!"
+
+// Handle the Promise returned from whatToEat here: 
+
+whatToEat('lunch')
+
+// Handle this Promise: 
+whatToEat('elevens')
