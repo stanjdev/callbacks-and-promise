ada08d69f9cbb36751eae7a3c228b8ae0e16434a
Mitchell Hudson <soggybag@gmail.com>
2023-01-24 18:48:10 -0800

    Update index.js

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -10,5 +10,5 @@ const food = new Promise((resolve, reject) => {
 // **Challenge 1** Resolve your promise with the name of your favorite food. 
 // Do this by calling the `resolve` with an argument. 
 
-// **Challenge 2** Resolve the `food` promise with that `.then()` syntax 
+// **Challenge 2** Resolve the promise `food` with the `.then()` syntax 
 // and print your favorite food to the console. 
